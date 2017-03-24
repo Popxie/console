9c27b2e3deb67b21baa43c5964e4dca5568f9fb2
chengfei <chengfei@mingbikes.com>
2017-03-24 19:31:44 +0800

    创建优惠券

--- src/views/CreateCoupon.vue
+++ src/views/CreateCoupon.vue
@@ -7,7 +7,7 @@
         <el-row v-if="showNext">
             <el-col :lg="{span: 11,offset:1}" :md="{span: 14, offset:1}" :sm="{span:16, offset:1}"
                     :xs="{span: 18, offset:1}">
-                <el-form class="mt40" ref="form" :model="form" :rules="rules" label-position="left" label-width="150px">
+                <el-form class="mt40" ref="form" :model="form" :rules="rules" label-position="left" label-width="170px">
                     <el-form-item label="是否仅新用户可用：" prop="isNewuserUse">
                         <el-radio-group v-model="form.isNewuserUse">
                             <el-radio :label="1">是</el-radio>
@@ -20,8 +20,6 @@
                             <el-radio :label="2">2</el-radio>
                             <el-radio :label="3">3</el-radio>
                             <el-radio :label="other">其他
-
-
                                 <el-input style="width: 40px;" v-model="other" @change="setLimit"
                                           placeholder="请输入内容"></el-input>
                             </el-radio>
@@ -29,10 +27,10 @@
                         </el-radio-group>
                     </el-form-item>
                     <el-form-item label="发券活动主题：" prop="topic">
-                        <el-input v-model="form.topic"></el-input>
+                        <el-input v-model="form.topic" :maxlength="30"></el-input>
                     </el-form-item>
                     <el-form-item label="优惠卷显示名称：" prop="couponName">
-                        <el-input v-model="form.couponName"></el-input>
+                        <el-input v-model="form.couponName" :maxlength="15"></el-input>
                     </el-form-item>
                     <el-form-item label="设置投放地域：" prop="areaType">
                         <el-radio-group v-model="form.areaType" @change="selectArea">
@@ -41,13 +39,18 @@
                         </el-radio-group>
                         <el-button v-show="form.areaType==1" type="text" @click="selectAreaByClick">
                             {{form.provinceName + '-' + form.cityName + '-' + form.areaName}}
+
                         </el-button>
                     </el-form-item>
-                    <el-form-item label="设置券的面额&数量：">
+                    <el-form-item v-if="form.type != 3" label="设置券的面额&数量：" required>
                         <div class="row-wrap">
                             <template v-for="(item, index)  in form.allDenomination">
                                 <div class="wid row">
-                                    <el-input v-model="item.allDenomination"></el-input>元劵<el-input v-model="item.totalDenomination"></el-input>张数
+                                    <el-input v-model="item.allDenomination" placeholder="输入劵面额"></el-input>
+                                    元劵
+                                    <el-input v-model="item.totalDenomination" placeholder="输入张数"></el-input>
+                                    张数
+
                                 </div>
                             </template>
                             <div class="wid">
@@ -55,18 +58,52 @@
                             </div>
                         </div>
                     </el-form-item>
-                    <el-form-item  label="设置劵的过期日期：" prop="time">
+                    <template v-if="form.type == 3">
+                        <el-form-item label="设置券的面额&数量：" required>
+                            <div class="row-wrap">
+                                <template v-for="(item, index)  in form.allDenomination">
+                                    <div class="wid row">
+                                        <el-input v-if="index == 0" @change="setAllDenomination" v-model="item.allDenomination" placeholder="输入折扣劵"></el-input>
+                                        <el-input v-if="index != 0" v-model="item.allDenomination" placeholder="输入折扣劵"></el-input>
+                                        元劵
+                                        <el-input v-model="item.totalDenomination" placeholder="输入张数"></el-input>
+                                        张数
+                                    </div>
+                                </template>
+                                <div class="wid">
+                                    <el-button icon="plus" @click="addCoupon"></el-button>
+                                </div>
+                            </div>
+                        </el-form-item>
+                        <el-form-item  label="设置券的最高抵扣金额：" prop="maxDeductionType">
+
+                            <el-radio-group v-model="form.maxDeductionType">
+                                <el-radio :label="0">统一上限
+                                    <el-input style="width: 40px;" v-model="form.maxDeductionMoney"
+                                              placeholder="输入金额"></el-input>
+                                </el-radio>
+                                <br>
+                                <el-radio :label="1">单独设置：
+                               <template v-for="item in form.allDenomination">
+                                    {{item.allDenomination || '--' }}元卷<el-input  style="width: 60px;" v-model="item.maxDeductionMoney"
+                                              placeholder="金额"></el-input>元
+                                </template>
+                                </el-radio>
+                            </el-radio-group>
+                        </el-form-item>
+                    </template>
+                    <el-form-item label="设置劵的过期日期：" prop="time">
                         <el-date-picker
                             v-model="form.time"
                             type="date"
-                            placeholder="选择日期"
-                            @change = "setEndTime"
+                            placeholder="过期时间"
+                            @change="setEndTime"
                             :picker-options="pickerOptions">
                         </el-date-picker>
                     </el-form-item>
                     <el-form-item>
                         <el-button type="info" @click="showNext = false">返回</el-button>
-                        <el-button type="info" @click="finishCreate('form')">完成</el-button>
+                        <el-button type="info" @click="finishCreate('form')">生成劵</el-button>
                     </el-form-item>
                 </el-form>
             </el-col>
@@ -76,6 +113,7 @@
 <script>
     import TapSelect from '../components/TapSelect.vue';
     import SelectAreas from '../components/SelectSimpleArea.vue';
+    import {settings} from '../config/settings';
     export default{
         components: {
             TapSelect, SelectAreas
@@ -100,6 +138,7 @@
                 showNext: false,
                 dialogVisible: false,
                 other: 0,
+                maxMoney: 0,
                 form: {
                     type: 0,
                     isNewuserUse: 1,
@@ -108,9 +147,22 @@
                     couponName: '',
                     areaType: 0,
                     allDenomination: [{}],
-                    time: ''
+                    time: '',
+                    maxDeductionType: 0,
+                    maxDeductionMoney: 0,
+                    denomination: 0
                 },
-                rules: {}
+                rules: {
+                    topic: [
+                        {required: true, message: '请输入广告主题', trigger: 'blur'},
+                    ],
+                    couponName: [
+                        {required: true,  message: '请输入优惠劵显示名称', trigger: 'blur'}
+                    ],
+                    time: [
+                        {type: 'date', required: true, message: '请选择结束时间', trigger: 'change'}
+                    ]
+                }
             }
         },
         methods: {
@@ -123,7 +175,12 @@
                 let self = this;
                 self.$nextTick(() => {
                     self.form.limitSize = self.other
-                })
+                });
+            },
+            setMaxMoney() {
+                this.$nextTick(() => {
+
+                });
             },
             selectArea() {
                 let self = this;
@@ -163,14 +220,40 @@
                 self.form.areaName = '';
             },
             addCoupon() {
-            	let self = this;
-            	self.form.allDenomination.push({});
+                let self = this;
+                self.form.allDenomination.push({});
             },
             setEndTime(val) {
                 this.form.endTime = val;
             },
-            finishCreate(form) {
-                console.log(this.form);
+            setAllDenomination(val) {
+                this.form.allDenomination.splice(0,1 ,{allDenomination: val});
+            },
+            finishCreate() {
+                let self = this;
+                self.$refs['form'].validate((valid)=> {
+                    if(valid) {
+                        self.$http.post(settings.URL, self.form)
+                            .then( (res)=> {
+                            	if(res.statusCode == 200) {
+                                    self.$notify({
+                                        title: '成功',
+                                        message: '创建成功',
+                                        type: 'success'
+                                    });
+                                    self.$router.push('couponList');
+                                }
+                            }, (err)=> {
+                                self.$notify({
+                                    title: '失败',
+                                    message: err,
+                                    type: 'error'
+                                });
+                            })
+                    }else {
+                    	return false
+                    }
+                })
             }
         }
     }
@@ -195,10 +278,11 @@
         justify-content: space-between;
         flex-wrap: wrap
     }
+
     .row {
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
-        white-space:nowrap;
+        white-space: nowrap;
     }
 </style>
