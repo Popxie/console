876fec55696319a4ad585b38844307154ed6d972
chengfei <chengfei@mingbikes.com>
2017-03-24 17:06:02 +0800

    创建优惠券

--- src/views/CreateCoupon.vue
+++ src/views/CreateCoupon.vue
@@ -1,9 +1,24 @@
 <template>
     <div class="container">
     <TapSelect v-if="!showNext" :taps="taps" title="优惠券管理" @setValue="setCoupon"/>
-    <el-row v-if="showNext">
+    <el-row  v-if="showNext">
         <el-col :lg="{span: 11,offset:1}" :md="{span: 14, offset:1}" :sm="{span:16, offset:1}" :xs="{span: 18, offset:1}">
-            <el-form ref="form" :model="form" :rules="rules" label-position="left" label-width="150px">
+            <el-form class="mt40" ref="form" :model="form" :rules="rules" label-position="left" label-width="150px">
+                <el-form-item label="是否仅新用户可用：" prop="isNewuserUse">
+                    <el-radio-group v-model="form.isNewuserUse">
+                        <el-radio :label="1">是</el-radio>
+                        <el-radio :label="0">否</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="本批次劵单用户可领用/兑换上限张数：" prop="limitSize">
+                    <el-radio-group v-model="form.limitSize">
+                        <el-radio :label="1">1</el-radio>
+                        <el-radio :label="2">2</el-radio>
+                        <el-radio :label="3">3</el-radio>
+                        <el-radio :label="other">其他<el-input style="width: 20px;" v-model="other" placeholder="请输入内容"></el-input></el-radio>
+                        <el-radio :label="-1">不限</el-radio>
+                    </el-radio-group>
+                </el-form-item>
             </el-form>
         </el-col>
     </el-row>
@@ -28,8 +43,11 @@
                     value: 3
                 }],
                 showNext: false,
+                other: 0,
                 form: {
-
+                    type: 0,
+                    isNewuserUse: 1,
+                    limitSize: 1
                 },
                 rules: {
 
@@ -40,6 +58,7 @@
             setCoupon(val) {
                 let self = this;
                 self.showNext = true;
+                self.form.type = val;
             }
         }
     }
@@ -47,5 +66,8 @@
 <style scoped>
     .container{
         background-color: #fff;
+    }
+    .mt40{
+        margin-top: 40px;
     }
 </style>
