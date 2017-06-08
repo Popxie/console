0c4e472b6d014ece3f5f69140f5a0d201f907357
陈伊珂 <chenyike@mingbikes.com>
2017-06-08 17:32:49 +0800

    修复 直接点全域不能选择所有区域的bug

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,11 +3,11 @@
  */
 export const settings = {
      // URL: 'https://api.mingbikes.com'
-     // URL: 'https://test.mingbikes.com'
+     URL: 'https://test.mingbikes.com'
     // URL: 'http://192.168.1.12
     // 1:8000',
     // URL: 'http://localhost:3000'
 
-    URL: 'http://192.168.6.3:8000'
+    // URL: 'http://192.168.6.3:8000'
 
 }
